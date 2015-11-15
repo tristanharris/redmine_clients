3728f66f8fb92fc9523fc9f27bbd9be48ef23a46
Tristan Harris <tris.dev@pumpcottage.me.uk>
2015-11-15 15:41:02 +0000

    Refactor js calls

--- lib/client_format.rb
+++ lib/client_format.rb
@@ -24,7 +24,7 @@ module Redmine
         text = cast_custom_value(custom_value)
         view.hidden_field_tag(tag_name, custom_value.value, options.merge(:id => tag_id)) +
           view.text_field_tag(nil, text, options.merge(:id => text_id, :data => {tagid: tag_id})) +
-          view.render(:partial => 'clients/autocomplete', :locals => {text_id: text_id})
+          view.javascript_tag("apply_autocomplete('#{text_id}');")
       end
 
       def bulk_edit_tag(view, tag_id, tag_name, custom_field, objects, value, options={})
@@ -33,7 +33,7 @@ module Redmine
         view.hidden_field_tag(tag_name, value, options.merge(:id => tag_id)) +
           view.text_field_tag(nil, text, options.merge(:id => text_id, :data => {tagid: tag_id})) +
           bulk_clear_tag(view, tag_id, tag_name, custom_field, value) +
-          view.render(:partial => 'clients/autocomplete', :locals => {text_id: text_id})
+          view.javascript_tag("apply_autocomplete('#{text_id}');")
       end
 
       def query_filter_options(custom_field, query)
