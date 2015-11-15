f399d1d5b20df566371465977967088d68ab10f4
Tristan Harris <tris.dev@pumpcottage.me.uk>
2015-11-15 16:00:21 +0000

    Fix name

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 require 'client_format'
 
-module MyPlugin
+module ClientsPlugin
   class Hooks < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context={ })
       return javascript_include_tag 'plugin', :plugin => 'clients'
